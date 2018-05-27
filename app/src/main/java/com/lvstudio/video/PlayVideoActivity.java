daa9653e95d7928ab496a9e6351de3b6d08d777f
lvstudio <lvchongstudio@gmail.com>
2018-05-27 18:57:08 +0800

    init

---
+++ app/src/main/java/com/lvstudio/video/PlayVideoActivity.java
@@ -0,0 +1,18 @@
+package com.lvstudio.video;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by lvchong on 2018/5/14.
+ */
+
+public class PlayVideoActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ijk);
+    }
+}
